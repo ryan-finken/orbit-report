7eb1beb98c7350327612a5ec99cd65c400434626
Ryan Finken <ryanfinken338@gmail.com>
2021-04-01 10:39:05 -0500

    finished parts 1-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,24 @@ export class AppComponent {
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function (response) {
+      response.json().then(function (data) {
+
+        let fetchedSatellites = data.satellites; // why no type declaration?
+        for (const satelliteJSON of fetchedSatellites) {
+          const satellite: object = new Satellite(
+            satelliteJSON.name,
+            satelliteJSON.type,
+            satelliteJSON.launchDate,
+            satelliteJSON.orbitType,
+            satelliteJSON.operational
+          );
+          this.sourceList.push(satellite);
+        }
+      }.bind(this));
+    }.bind(this));
   }
 }
